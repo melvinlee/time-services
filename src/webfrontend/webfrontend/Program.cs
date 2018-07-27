3614197bae714e1eac86ba11b2b3ffcd91bcadee
melvinlee <melvinlee@hotmail.com>
2018-07-28 00:54:57 +0800

    feat: added readiness and liveness probes.

--- src/webfrontend/webfrontend/Program.cs
+++ src/webfrontend/webfrontend/Program.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 
 namespace webfrontend
 {
@@ -19,6 +12,11 @@ namespace webfrontend
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                // Kubernetes Liveness and Readiness Probes
+                // The kubelet uses readiness probes to know when a Container is ready to start accepting traffic.
+                .UseHealthChecks("/readiness")
+                // The kubelet uses liveness probes to know when to restart a Container.
+                .UseHealthChecks("/liveness")
                 .UseStartup<Startup>();
     }
 }
--- src/webfrontend/webfrontend/Startup.cs
+++ src/webfrontend/webfrontend/Startup.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.HealthChecks;
+using System.Threading.Tasks;
 
 namespace webfrontend
 {
@@ -18,6 +20,12 @@ namespace webfrontend
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddHealthChecks(checks =>
+            {
+                checks.AddValueTaskCheck("HTTP Endpoint", () => new
+                    ValueTask<IHealthCheckResult>(HealthCheckResult.Healthy("Ok")));
+            });
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
