43a566d433e98b7e9bf60aa52f0a6e86b49273c3
melvinlee <melvinlee@hotmail.com>
2018-07-28 01:36:54 +0800

    fix: fixed namespaces.

--- src/backend/backend/Startup.cs
+++ src/backend/backend/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.HealthChecks;
+using System.Threading.Tasks;
 
 namespace backend
 {
