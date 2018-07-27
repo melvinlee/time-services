dd6f6eed4038e5f622cc869696bd034f696a568a
melvinlee <melvinlee@hotmail.com>
2018-07-28 01:05:05 +0800

    feat: added backend services.

---
+++ src/backend/backend/Controllers/ValuesController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace backend.Controllers
+{
+    [Route("api/")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        public readonly IConfiguration _configuration;
+
+        public ValuesController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+
+        // GET api/values/5
+        [HttpGet()]
+        public ActionResult<string> Get()
+        {
+            var service_name = _configuration.GetValue<string>("SERVICE_NAME");
+            var version = _configuration.GetValue<string>("VERSION") ?? "v1";
+
+            return $"backend-{service_name}:{version}";
+        }
+    }
+}
