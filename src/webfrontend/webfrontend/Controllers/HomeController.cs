aca875e35b929fdb299f047eb623952feb871779
melvinlee <melvinlee@hotmail.com>
2018-07-28 00:35:33 +0800

    feat: added webfrontned.

---
+++ src/webfrontend/webfrontend/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System;
+using Microsoft.Extensions.Configuration;
+
+namespace webfrontend.Controllers
+{
+    [Route("/")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        public readonly IConfiguration _configuration;
+
+        public HomeController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        // GET: /<controller>/
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Index()
+        {
+            var version = _configuration.GetValue<string>("version") ?? "v1";
+
+            return new string[] { $"frontend:{version} (host: {Environment.MachineName})"};
+        }
+
+    }
+}
